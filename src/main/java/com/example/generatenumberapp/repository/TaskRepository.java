65a0c05eb3fccf3073fad239da7852dbb3584534
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-12-04 17:35:28 +0100

    commit

--- src/main/java/com/example/generatenumberapp/repository/TaskRepository.java
+++ src/main/java/com/example/generatenumberapp/repository/TaskRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 
