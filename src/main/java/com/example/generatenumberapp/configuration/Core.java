33bf38d6dcbcdeb0a8808c2cc1e18e302c86c848
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-12-08 20:15:39 +0100

    add SpringSecurityConfig

--- src/main/java/com/example/generatenumberapp/configuration/Core.java
+++ src/main/java/com/example/generatenumberapp/configuration/Core.java
@@ -6,12 +6,25 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.Executor;
 
+@EnableWebMvc
 @Configuration
 @EnableAsync
 @EnableScheduling
+@EnableSwagger2
 public class Core {
 
     @Bean(name = "asyncExecutor")
@@ -23,5 +36,27 @@ public class Core {
         executor.setThreadNamePrefix("AsynchThread-");
         executor.initialize();
         return executor;
+    }
+
+    @Bean
+    public Docket getDocket () {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.generatenumberapp.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo(
+                "Generate Random App",
+                "app to random value",
+                "1.0",
+                "http",
+                new Contact("Grzegorz", "url", "mymail"),
+                "my licence",
+                "new URL",
+                Collections.emptyList()
+        );
     }
 }
